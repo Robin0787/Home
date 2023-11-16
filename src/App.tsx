5e1a17bbcdf03424a2fa0c056b4979238826918b
Robin <mohammadrobin636@gmail.com>
2023-11-17 00:32:42 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-black text-white flex justify-center items-center">
+      <h1 className="text-9xl">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
